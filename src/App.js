9b9f57e973fbb88d41fd714520b396cff971ebd2
Sachin Chopra <sachin.chopra@grabtaxi.com>
2019-11-04 20:06:09 +0800

    Revert "add a basic bot"
    This reverts commit 03ab4a7764b32933a1e9950c2a1ec1e804feeef5.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css'
-import {sayHello, ackHello, converse, changeColor} from "./bot";
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
-      {
-        conversation()
-      }
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-}
-
-function conversation() {
-  sayHello().then(() =>
-      ackHello().then(() =>
-          converse()
-      ).then(() =>
-          changeColor()
-      )
-  )
 }
 
 export default App;
